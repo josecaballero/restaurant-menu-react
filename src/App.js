fe320c9f20333016dd0140b7b481b9673678723d
Jose <jlcab8@gmail.com>
2022-05-20 11:54:14 +0200

    filter categories dinamically

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+// new Set selecciona solo las categorías únicas
+const allCategories = ["All", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "All") {
+      setMenuItems(items);
+      return;
+    }
+    // we filter the original list (items) because that will not change.
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -14,9 +26,9 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
 
-        <Menu menus={menuItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
--- src/Menu.js
+++ src/Menu.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Menu = ({ menus }) => {
+const Menu = ({ items }) => {
   return (
     <div className="section-center">
-      {menus.map((menu) => {
-        const { id, title, category, price, img, desc } = menu;
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
         return (
           <article key={id} className="menu-item">
             <img src={img} alt={title} className="photo" />
             <div className="item-info">
               <header>
                 <h4>{title}</h4>
-                <h4 className="price">{price}</h4>
+                <h4 className="price">${price}</h4>
               </header>
               <p className="item-text">{desc}</p>
             </div>
